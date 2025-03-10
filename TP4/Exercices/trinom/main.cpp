36b62b5766fbbdc841aeae24cd7ac4ad5403bb54
alvalade <alvalade@ims-bordeaux.fr>
2025-03-10 14:53:32 +0100

    Debut trinom

---
+++ TP4/Exercices/trinom/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author Alexandre Valade (alvalade@enseirb-matmeca.fr)
+ * @brief 
+ * @version 0.1
+ * @date 2025-03-10
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+}
